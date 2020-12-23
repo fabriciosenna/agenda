8435c523a4651e8972e4b8f40b200010c5ecf730
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-23 14:45:40 -0300

    Refatorando as implementações

---
+++ Agenda/Componentes/Safari.swift
@@ -0,0 +1,29 @@
+//
+//  Safari.swift
+//  Agenda
+//
+//  Created by Fabricio Rodrigo Baixo on 12/23/20.
+//  Copyright © 2020 Alura. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class Safari: NSObject {
+
+    func abrePaginaWeb(_ alunoSelecionado: Aluno,controller:UIViewController){
+        if let urlDoAluno = alunoSelecionado.site {
+            
+            var urlFormatada = urlDoAluno
+            
+            if !urlFormatada.hasPrefix("http://") {
+                urlFormatada = String(format: "http://%@", urlFormatada)
+            }
+            
+            guard let url = URL(string: urlFormatada) else { return }
+            let safariViewController = SFSafariViewController(url: url)
+            controller.present(safariViewController,animated: true, completion: nil)
+        }
+
+    }
+}
